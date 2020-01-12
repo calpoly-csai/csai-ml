15d5ed45db3a83b25df918ec0421f47222107114
RichaGadgil <gadgilricha@gmail.com>
2020-01-12 11:51:08 -0800

    add comments for clarification

--- feature-creation.py
+++ feature-creation.py
@@ -7,16 +7,25 @@ from sklearn import preprocessing
 from sklearn.neighbors import KNeighborsClassifier
 
 def get_question_features(question):
+    """
+    Method to extract features from each individual question.
+    """
     features = {}
+
+    # ADD ALL VARIABLES TO THE FEATURE DICT WITH A WEIGHT OF 90
     matches = re.findall(r'(\[(.*?)\])', question)
     for match in matches:
         question = question.replace(match[0], '')
         features[match[0]] = 90
     question = re.sub('[^a-zA-Z0-9]', ' ', question)
+
+    # PRE-PROCESSING: TOKENIZE SENTENCE, AND LOWER AND STEM EACH WORD
     words = nltk.word_tokenize(question)
     words = [word.lower() for word in words if '[' and ']' not in word]
     porter_stemmer = nltk.stem.porter.PorterStemmer()
     filtered_words = [porter_stemmer.stem(word) for word in words]
+
+    # ADD FIRST WORD AND NON-STOP WORDS TO FEATURE DICT
     features[filtered_words[0]] = 30
     filtered_words = [word for word in filtered_words if word not in nltk.corpus.stopwords.words('english')]
     for word in filtered_words:
@@ -24,10 +33,17 @@ def get_question_features(question):
     return features
 
 def build_question_classifier():
+    """
+    Build overall feature set for each question based on feature vectors of individual questions.
+    Train KNN classification model with overall feature set.
+    """
+
+    # READ QUESTIONS
     questions = pd.read_csv('questions.csv')
     questions['features'] = questions['questionFormat'].apply(get_question_features)
     question_features = questions['features'].values.tolist()
 
+    # BUILD OVERALL FEATURE SET FROM INDIVIDUAL QUESTION FEATURE VECTORS
     overall_features = {}
     for feature in question_features:
         for key in feature:
@@ -49,6 +65,10 @@ def build_question_classifier():
     return classifier, overall_features
 
 def classify_question(test, overall_features, classifier):
+    """
+    Match a user query with a question in the database based on the classifier we trained and overall features we calculated.
+    Return relevant question.
+    """
     test_features = get_question_features(test)
     test_vector = dict.fromkeys(overall_features, 0)
     for key in test_features:
